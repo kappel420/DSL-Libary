4bf7739491accd185f7c957291593f2f57a943b7
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-06 14:08:35 +0200

    added pipelineMaven

---
+++ vars/pipelineMaven.groovy
@@ -0,0 +1,43 @@
+def call(Map settings = [:]) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Get Source Code') {
+                steps {
+                    // pobiera kod z mastera
+                    git branch: settings.branch ?: 'master', url: settings.repository
+                }
+            }
+
+            stage('Build with Maven') {
+                steps {
+                    // buduje mavena
+                    sh 'mvn package -DskipTests'
+                }
+            }
+
+            stage('Run Tests') {
+                steps {
+                    script {
+                        try {
+                            if (!settings.skipTests) {
+                                sh 'mvn verify'
+                                junit 'target/surefire-reports/*.xml'
+                            }
+                        } catch (Exception e) {
+                            archiveArtifacts allowEmptyArchive: true, artifacts: 'target/surefire-reports/*.xml'
+                        }
+                    }
+                }
+            }
+
+            stage('Install Artifact') {
+                steps {
+                    // instaluje artefakt
+                    sh 'mvn install -DskipTests'
+                }
+            }
+        }
+    }
+}
