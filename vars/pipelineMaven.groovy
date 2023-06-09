b233ae36242243487d505874c0d91a1b0409867e
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-09 12:50:54 +0200

    asasas

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,43 +1,35 @@
 // DSL-Library/src/org/example/PipelineDSL.groovy
 
-def call(Map config) {
-    def repository = config.repository
-    def branch = config.branch
-    def skipTests = config.skipTests ?: false
-    def skipInstall = config.skipInstall ?: false
-
+def call(Map config = [:]) {
     pipeline {
-        agent none
+        agent {
+            label 'tomek'
+        }
         stages {
-            stage('Example') {
-                agent any
+            stage('Fetch Source Code') {
                 steps {
-                    script {
-                        echo "test"
-                    }
-                    script {
-                        pipelineMavenStep(repository, branch, skipTests, skipInstall)
-                    }
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: config.branch ?: 'master']],
+                        userRemoteConfigs: [[url: config.repository ?: '']],
+                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'mvn package -DskipTests'
+                }
+            }
+            stage('Run Tests') {
+                steps {
+                    sh 'mvn verify'
+                    junit '**/target/surefire-reports/*.xml'
+                }
+            }
+            stage('Install Artifact') {
+                steps {
+                    sh 'mvn install -DskipTests'
                 }
             }
         }
-    }
-}
-
-def pipelineMavenStep(repository, branch, skipTests, skipInstall) {
-    stage('Fetch Source Code') {
-        // Code to fetch source code from the repository
-    }
-
-    stage('Build') {
-        // Code to build the project using 'mvn package -DskipTests'
-    }
-
-    stage('Run Tests') {
-        // Code to run tests using 'mvn verify' and import JUnit results
-    }
-
-    stage('Install Artifact') {
-        // Code to install artifact in local repository using 'mvn install -DskipTests'
     }
 }
