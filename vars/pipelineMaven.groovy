5b3eb104b40d5b05bbae24f737230452b30bb74c
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 09:59:53 +0200

    qweeqwewq

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -20,36 +20,32 @@ def call(Map config = [:]) {
         stages {
             stage('Fetch Source Code') {
                 steps {
-                        checkout([$class: 'GitSCM',
-                            branches: [[name: config.branch ?: 'main']],
-                            userRemoteConfigs: [[url: config.repository ?: '']],
-                            extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
-                    }
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: config.branch ?: 'main']],
+                        userRemoteConfigs: [[url: config.repository ?: '']],
+                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
                 }
             }
             stage('Build') {
                 steps {
-                        sh 'mvn package -DskipTests'
-                    }
+                    sh 'mvn package -DskipTests'
                 }
             }
             stage('Run Tests') {
                 steps {
-                        script {
-                            if (!config.skipTests) {
-                        sh 'mvn verify'
-                        junit '**/target/surefire-reports/*.xml'
-                    }
+                    sh 'mvn verify'
+                    junit '**/target/surefire-reports/*.xml'
                 }
-            }
-            }
             }
             stage('Install Artifact') {
                 steps {
-                        script {
-                            if (!config.skipInstall) {
-                                sh 'mvn install -DskipTests'
-                            }
+                    script {
+                        if (!config.skipInstall) {
+                            sh 'mvn install -DskipTests'
                         }
                     }
                 }
+            }
+        }
+    }
+}
