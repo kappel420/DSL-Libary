addaabed3ea163f90f900da39594bf62307ba6c1
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 12:49:27 +0200

    asas

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,3 +1,30 @@
+def call(Map config = [:], String a) {
+    node {
+    stage('Fetch Source Code') {
+        checkout scm
+    }
+
+    stage('Build') {
+        sh 'mvn package -DskipTests'
+        echo "dupa"
+    }
+
+    stage('Run Tests') {
+        if (params.skipTests) {
+            sh 'mvn verify'
+            junit '**/target/surefire-reports/*.xml'
+        }
+    }
+
+    stage('Install Artifact') {
+        if (!params.skipInstall) {
+            sh 'mvn install -DskipTests'
+        }
+    }
+}
+}
+
+
 def call(Map config = [:]) {
     node {
     stage('Fetch Source Code') {
