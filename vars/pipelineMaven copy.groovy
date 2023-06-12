33ce1d271f48e2404053edcc28db70ad2c60eec4
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 10:28:32 +0200

    aaa

---
+++ vars/pipelineMaven copy.groovy
@@ -0,0 +1,55 @@
+def call(Closure body) {
+    node {
+        wrap([$class: 'AnsiColorBuildWrapper']) {
+            wrap([$class: 'TimestamperBuildWrapper']) {
+                try {
+                    body()
+                } finally {
+                    cleanWs()
+                }
+            }
+        }
+    }
+}
+
+def call(Map config = [:], Closure body) {
+    pipeline {
+        agent {
+            label 'tomek'
+        }
+        stages {
+            stage('Fetch Source Code') {
+                steps {
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: config.branch ?: 'main']],
+                        userRemoteConfigs: [[url: config.repository ?: '']],
+                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'mvn package -DskipTests'
+                }
+            }
+            stage('Run Tests') {
+                steps {
+                    script{
+                        if (!config.skipTests) {
+                            sh 'mvn verify'
+                            junit '**/target/surefire-reports/*.xml'
+                        }
+                    }
+                }
+            }
+            stage('Install Artifact') {
+                steps {
+                    script {
+                        if (!config.skipInstall) {
+                            sh 'mvn install -DskipTests'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,55 +1,58 @@
-def call(Closure body) {
-    node {
-        wrap([$class: 'AnsiColorBuildWrapper']) {
-            wrap([$class: 'TimestamperBuildWrapper']) {
-                try {
-                    body()
-                } finally {
-                    cleanWs()
-                }
-            }
-        }
+def myNode(Closure body) {
+  node {
+    ansiColor('xterm') {
+      wrap([$class: 'TimestamperBuildWrapper']) {
+        body.call()
+      }
     }
+  }
 }
 
 def call(Map config = [:], Closure body) {
-    pipeline {
-        agent {
-            label 'tomek'
-        }
-        stages {
-            stage('Fetch Source Code') {
-                steps {
-                    checkout([$class: 'GitSCM',
-                        branches: [[name: config.branch ?: 'main']],
-                        userRemoteConfigs: [[url: config.repository ?: '']],
-                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
-                }
-            }
-            stage('Build') {
-                steps {
-                    sh 'mvn package -DskipTests'
-                }
-            }
-            stage('Run Tests') {
-                steps {
-                    script{
-                        if (!config.skipTests) {
-                            sh 'mvn verify'
-                            junit '**/target/surefire-reports/*.xml'
-                        }
-                    }
-                }
-            }
-            stage('Install Artifact') {
-                steps {
-                    script {
-                        if (!config.skipInstall) {
-                            sh 'mvn install -DskipTests'
-                        }
-                    }
-                }
-            }
-        }
+  pipeline {
+    agent {
+      label 'tomek'
     }
+    stages {
+      myNode {
+        stage('Fetch Source Code') {
+          steps {
+            checkout([$class: 'GitSCM',
+              branches: [[name: config.branch ?: 'main']],
+              userRemoteConfigs: [[url: config.repository ?: '']],
+              extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+          }
+        }
+        stage('Build') {
+          steps {
+            sh 'mvn package -DskipTests'
+          }
+        }
+        stage('Run Tests') {
+          steps {
+            script {
+              if (!config.skipTests) {
+                sh 'mvn verify'
+                junit '**/target/surefire-reports/*.xml'
+              }
+            }
+          }
+        }
+        stage('Install Artifact') {
+          steps {
+            script {
+              if (!config.skipInstall) {
+                sh 'mvn install -DskipTests'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  post {
+    always {
+      cleanWs()
+    }
+  }
 }
