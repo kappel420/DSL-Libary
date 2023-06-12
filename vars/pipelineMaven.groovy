9e546fe92fbcb80fb1391e939955617815f42d40
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 11:23:40 +0200

    asdasd

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,19 +1,4 @@
-def tester(Map config = [:], Closure body) {
-    def script = CpsScript.current()
-    script.node {
-        wrap([$class: 'AnsiColorBuildWrapper']) {
-            wrap([$class: 'TimestamperBuildWrapper']) {
-                try {
-                    body()
-                } finally {
-                    script.cleanWs()
-                }
-            }
-        }
-    }
-}
-
-def call(Map config) {
+def call(Map config = [:]) {
     pipeline {
         agent {
             label 'tomek'
@@ -50,6 +35,59 @@ def call(Map config) {
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+
+def calll(Map config = [:], String a) {
+    pipeline {
+        agent {
+            label 'tomek'
+        }
+        stages {
+            stage('Fetch Source Code') {
+                steps {
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: config.branch ?: 'main']],
+                        userRemoteConfigs: [[url: config.repository ?: '']],
+                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'mvn package -DskipTests'
+                }
+            }
+            stage('Run Tests') {
+                steps {
+                    script{
+                        if (!config.skipTests) {
+                            sh 'mvn verify'
+                            junit '**/target/surefire-reports/*.xml'
+                        }
+                    }
+                }
+            }
+            stage('Install Artifact') {
+                steps {
+                    script {
+                        if (!config.skipInstall) {
+                            sh 'mvn install -DskipTests'
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+        wrap([$class: 'TimestamperBuildWrapper']) {
+            try {
+                body()
+            } finally {
+                cleanWs()
             }
         }
     }
