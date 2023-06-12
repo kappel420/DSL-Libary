33ed070ceba7712023699ab86131a0d659c076ed
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 11:04:47 +0200

    a

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,49 +1,41 @@
 def call(Map config = [:]) {
     wrap([$class: 'TimestamperBuildWrapper']) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
-            node {
                 try {
                     pipelineBody(config)
                 } finally {
                     cleanWs()
                 }
-            }
         }
     }
 }
 
-def pipelineBody(Map config = [:]) {
+def pipelineBody(Map config) {
+    // Your existing pipeline code here
     pipeline {
         agent {
-            node {
-                label 'tomek'
-            }
+            label 'tomek'
         }
+        stages {
             stage('Fetch Source Code') {
                 steps {
-                    node {
-                        checkout([$class: 'GitSCM',
-                            branches: [[name: config.branch ?: 'main']],
-                            userRemoteConfigs: [[url: config.repository ?: '']],
-                            extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
-                    }
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: config.branch ?: 'main']],
+                        userRemoteConfigs: [[url: config.repository ?: '']],
+                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
                 }
             }
             stage('Build') {
                 steps {
-                    node {
-                        sh 'mvn package -DskipTests'
-                    }
+                    sh 'mvn package -DskipTests'
                 }
             }
             stage('Run Tests') {
                 steps {
                     script{
-                        node {
-                            if (!config.skipTests) {
-                                sh 'mvn verify'
-                                junit '**/target/surefire-reports/*.xml'
-                            }
+                        if (!config.skipTests) {
+                            sh 'mvn verify'
+                            junit '**/target/surefire-reports/*.xml'
                         }
                     }
                 }
@@ -51,13 +43,12 @@ def pipelineBody(Map config = [:]) {
             stage('Install Artifact') {
                 steps {
                     script {
-                        node {
-                            if (!config.skipInstall) {
-                                sh 'mvn install -DskipTests'
-                            }
+                        if (!config.skipInstall) {
+                            sh 'mvn install -DskipTests'
                         }
                     }
                 }
             }
         }
     }
+}
