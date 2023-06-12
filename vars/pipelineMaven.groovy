07edbf918f67cf31ef75b2b4d424c44d3381e637
kacper.solecki <kacper.solecki@onwelo.com>
2023-06-12 08:48:19 +0200

    assas

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,13 +1,11 @@
-// DSL-Library/src/org/example/PipelineDSL.groovy
-def call(Closure body) {
-    // Overriding the 'node' step
+def customNode(Closure body) {
     node {
         wrap([$class: 'AnsiColorBuildWrapper']) {
             wrap([$class: 'TimestamperBuildWrapper']) {
                 try {
                     body()
                 } finally {
-                    step([$class: 'WsCleanup'])
+                    cleanWs()
                 }
             }
         }
@@ -22,28 +20,36 @@ def call(Map config = [:]) {
         stages {
             stage('Fetch Source Code') {
                 steps {
-                    checkout([$class: 'GitSCM',
-                        branches: [[name: config.branch ?: 'main']],
-                        userRemoteConfigs: [[url: config.repository ?: '']],
-                        extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+                    customNode {
+                        checkout([$class: 'GitSCM',
+                            branches: [[name: config.branch ?: 'main']],
+                            userRemoteConfigs: [[url: config.repository ?: '']],
+                            extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', honorRefspec: false]]])
+                    }
                 }
             }
             stage('Build') {
                 steps {
-                    sh 'mvn package -DskipTests'
+                    customNode {
+                        sh 'mvn package -DskipTests'
+                    }
                 }
             }
             stage('Run Tests') {
                 steps {
-                    sh 'mvn verify'
-                    junit '**/target/surefire-reports/*.xml'
+                    customNode {
+                        sh 'mvn verify'
+                        junit '**/target/surefire-reports/*.xml'
+                    }
                 }
             }
             stage('Install Artifact') {
                 steps {
-                    script {
-                        if (!config.skipInstall) {
-                            sh 'mvn install -DskipTests'
+                    customNode {
+                        script {
+                            if (!config.skipInstall) {
+                                sh 'mvn install -DskipTests'
+                            }
                         }
                     }
                 }
